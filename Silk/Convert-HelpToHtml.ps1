5b186ec2ebcaef781160e6251e017445972f4f66
Aaron Jensen <ajensen@webmd.net>
2013-07-05 16:40:26 -0700

    Version 0.1.0 of Silk.

---
+++ Silk/Convert-HelpToHtml.ps1
@@ -0,0 +1,247 @@
+
+filter Convert-HelpToHtml 
+{
+    <#
+    .SYNOPSIS
+    Converts a command's help topic to an HTML page.
+    #>
+	param(
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true)]
+        # The command to document.
+        $CommandHelp,
+        
+        [Parameter(Mandatory=$true)]
+        [string]
+        # The menu to show on every page.
+        $Menu,
+
+        [Parameter(Mandatory=$true)]
+        # The configuration being used to generated this site.
+        $Config,
+
+        [Parameter(Mandatory=$true)]
+        # The path where the output should be saved.
+        $DestinationPath
+    )
+
+    $name = $CommandHelp.Name #| Format-ForHtml
+    $synopsis = $CommandHelp.Synopsis | Convert-MarkdownToHtml
+    $syntax = $CommandHelp.Syntax | Out-HtmlString | Format-ForHtml | ForEach-Object { $_ -split "`n" }
+    if( $syntax )
+    {
+        $syntax = @"
+    <h2>Syntax</h2>
+    <pre class="Syntax"><code>{0}</code></pre>
+"@ -f ($syntax -join "</code></pre>`n<pre class=""Syntax""><code>")
+    }
+    
+    $description = $CommandHelp.Description | Out-HtmlString | Convert-MarkdownToHtml
+    if( $description )
+    {
+        $description = @"
+    <h2>Description</h2>
+    <div class="Description">
+        $description
+    </div>
+"@
+    }
+    
+    $relatedCommands = $CommandHelp.RelatedLinks |
+        Out-String -Width ([Int32]::MaxValue) |
+        ForEach-Object { $_ -split "`n" } |
+        ForEach-Object { $_.Trim() } |
+        Where-Object { $_ } |
+        ForEach-Object {
+            if( $_ -match '^https?\:\/\/' )
+            {
+                "[{0}]({1})" -f $_,$_
+            }
+            else
+            {
+                "[{0}]({0}.html)" -f $_
+            }
+        }
+    
+    if( $relatedCommands )
+    {
+        $relatedCommands = @( $relatedCommands )
+        if( $relatedCommands.Length -gt 0 )
+        {
+            $relatedCommands = " * {0}" -f ($relatedCommands -join "`n * ")
+        }
+        $relatedCommands = @"
+        <h2>Related Commands</h2>
+        {0}
+"@ -f ($relatedCommands | Convert-MarkdownToHtml)
+    }
+    
+    $hasCommonParameters = $false
+    $parameters = $CommandHelp.Parameters.Parameter |
+        Where-Object { $_ } | 
+        ForEach-Object {
+        $commonParameterNames = @{
+                                'Verbose' = $true;
+                                'Debug' = $true;
+                                'WarningAction' = $true;
+                                'WarningVariable' = $true;
+                                'ErrorAction' = $true;
+                                'ErrorVariable' = $true;
+                                'OutVariable' = $true;
+                                'OutBuffer' = $true;
+                                'WhatIf' = $true;
+                                'Confirm' = $true;
+                             }
+            if( $commonParameterNames.ContainsKey( $_.name ) )
+            {
+                $hasCommonParameters = $true
+            }
+            
+            $typeDisplayName = $_.type.name
+            $typeName = $typeDisplayName
+            if( $typeName.EndsWith('[]') )
+            {
+                $typeName = $typeName -replace '\[\]',''
+            }
+            $typeFullName = $loadedTypes[$typeName]
+            $typeLink = $typeDisplayName
+            if( -not $typeFullName )
+            {
+                Write-Warning ("Type {0} not found." -f $_.type.name)
+            }
+            else
+            {
+                $typeLink = '<a href="http://msdn.microsoft.com/en-us/library/{0}.aspx">{1}</a>' -f $typeFullName.ToLower(),$typeDisplayName
+            }
+            $paramDescription = $_.Description | 
+                            Out-HtmlString | 
+                            Convert-MarkdownToHtml | 
+                            ForEach-Object { $_.Replace('<p>','').Replace('</p>','') }
+            @"
+			<tr valign='top'>
+				<td>{0}</td>
+				<td>{1}</td>
+				<td>{2}</td>
+				<td>{3}</td>
+				<td>{4}</td>
+                <td>{5}</td>
+			</tr>
+"@ -f $_.Name,$typeLink,$paramDescription,$_.Required,$_.PipelineInput,$_.DefaultValue
+        }
+        
+    if( $parameters )
+    {
+        $commonParameters = ''
+        if( $hasCommonParameters )
+        {
+            $commonParameters = @"
+                <tr valign="top">
+                <td><a href="http://technet.microsoft.com/en-us/library/dd315352.aspx">CommonParameters</a></td>
+                <td></td>
+                <td>This cmdlet supports common parameters.  For more information type <br> <code>Get-Help about_CommonParameters</code>.</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                </tr>
+"@
+        }
+        $parameters = @"
+		<h2> Parameters </h2>
+		<table border='1'>
+			<tr>
+				<th>Name</th>
+                <th>Type</th>
+				<th>Description</th>
+				<th>Required?</th>
+				<th>Pipeline Input</th>
+				<th>Default Value</th>
+			</tr>
+            {0}
+            {1}
+        </table>
+"@ -f ($parameters -join "`n"),$commonParameters
+    }
+
+    $inputTypes = $CommandHelp.inputTypes | Out-HtmlString
+    if( $inputTypes )
+    {
+        $inputTypes = @"
+        <h2>Input Type</h2>
+        <div>{0}</div>
+"@ -f $inputTuypes
+    }
+    
+    $returnValues = ($commandHelp.returnValues | Out-HtmlString) -replace "`n",' '
+    if( $returnValues )
+    {
+        if( $returnValues -match '^(.*?)\.(\s+(.*))?$' )
+        {
+            $type = [Type]$matches[1]
+            if( -not $type )
+            {
+                Write-Warning ("Type {0} not found." -f $matches[1])
+            }
+            $returnValues = '[{0}](http://msdn.microsoft.com/en-us/library/{1}.aspx). {2}' -f $type.FullName,$type.FullName.ToLower(),$matches[2]
+        }
+        else
+        {
+            Write-Warning "Unable to find type name in $returnValues.  Return value full type name should end with a period."
+        }
+        $returnValues = $returnValues | Convert-MarkdownToHtml
+        $returnValues = @"
+        <h2>Return Values</h2>
+        {0}
+"@ -f $returnValues
+    }
+    
+    $notes = $CommandHelp.AlertSet | Out-HtmlString
+    if( $notes )
+    {
+        $notes = @"
+        <h2>Notes</h2>
+        <div>{0}</div>
+"@ -f $notes
+    }
+    
+    $examples = $CommandHelp.Examples.example |
+        Where-Object { $_ } |
+        ForEach-Object {
+            @"
+            <h2>{0}</h2>
+            <pre><code>{1}</code></pre>
+            <p>{2}</p>
+"@ -f $_.title.Trim(('-',' ')),($_.code | Out-HtmlString),(($_.remarks | Out-HtmlString | Convert-MarkdownToHtml) -join '</p><p>')
+        }
+    
+@"
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <title>$name $($config.Title)</title>
+	<link href="styles.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+    $Menu
+
+    <h1>$name</h1>
+    <div>$synopsis</div>
+
+    $syntax
+    
+    $description
+    
+    $relatedCommands
+
+    $parameters
+        
+    $inputTypes
+        
+    $returnValues
+        
+    $notes
+        
+    $examples
+</body>
+</html>   
+"@ | Out-File -FilePath (Join-Path $DestinationPath ("{0}.html" -f $CommandHelp.Name)) -Encoding OEM
+}
+
---
+++ Silk/New-ModuleWebsite.ps1
@@ -0,0 +1,114 @@
+<#
+.SYNOPSIS
+Creates a new website for a module, using the module's help topics.
+
+.DESCRIPTION
+
+.EXAMPLE
+New-ModuleWebsite -ConfigFilePath silk.json -DestinationPath C:\Inetpub\wwwroot\MyModule
+
+Uses the `silk.json` configuration file to publish a module to `C:\Inetpub\wwwroot\MyModule`
+#>
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [string]
+    # The path to the Silk configuration file to use.
+    $ConfigFilePath,
+
+    [Parameter(Mandatory=$true)]
+    [string]
+    # The path where the module's help should be published.
+    $DestinationPath
+)
+
+$Error.Clear()
+Set-StrictMode -Version Latest
+$PSScriptRoot = Split-Path -Parent -Path $PSCommandPath
+
+& (Join-Path $PSScriptRoot Import-Silk.ps1 -Resolve)
+
+if( -not (Test-Path -Path $ConfigFilePath -PathType Leaf) )
+{
+    Write-Error ('Silk configuration file <{0}> not found.' -f $ConfigFilePath)
+    exit 1
+}
+
+$ConfigFilePath = Resolve-Path -Path $ConfigFilePath | Select-Object -ExpandProperty ProviderPath
+$ConfigFileRoot = Split-Path -Parent -Path $ConfigFilePath
+
+$config = [IO.File]::ReadAllText( $ConfigFilePath ) | ConvertFrom-Json
+if( -not $config )
+{
+    Write-Error ('Invalid Silk configuration file <{0}>.' -f $ConfigFilePath)
+    exit 1
+}
+
+$modulePath = $config.ModulePath
+if( -not ([IO.Path]::IsPathRooted($modulePath) ) )
+{
+    $modulePath = Join-Path $ConfigFileRoot $modulePath
+}
+
+if( -not (Test-Path -Path $modulePath) )
+{
+    Write-Error ('ModulePath <{0}> in Silk configuration file <{1}> not found. Relative paths should be relative to the configuration file itself.' -f $modulePath,$ConfigFilePath)
+    exit 1
+}
+
+$moduleName = $config.ModuleName
+if( (Get-Module $moduleName) )
+{
+    Remove-Module $moduleName
+}
+Import-Module $modulePath
+
+if( -not (Get-Module $moduleName) )
+{
+    Write-Error ('Failed to load module <{0}> from <{1}>.' -f $moduleName,$modulePath)
+    exit 1
+}
+
+$commands = Get-Command -Module $moduleName | 
+                Where-Object { $_.ModuleName -eq $moduleName -and $_.Name } | 
+                Sort-Object Name 
+
+$menuBuilder = New-Object Text.StringBuilder
+[void] $menuBuilder.AppendFormat( @"
+	<ul id="SiteNav">
+		<li>{0}</li>
+	</ul>"@, $moduleName )
+[void] $menuBuilder.AppendLine( '<div id="CommandMenuContainer" style="float:left;">' )
+[void] $menuBuilder.AppendFormat( "`t<ul class=""CommandMenu"">`n" )
+$commands | 
+    Where-Object { $config.CommandsToSkip -notcontains $_ } |
+    ForEach-Object {
+        [void] $menuBuilder.AppendFormat( "`t`t<li><a href=""{0}.html"">{0}</a></li>", $_.Name )
+    }
+[void] $menuBuilder.AppendLine( "`t</ul>" )
+[void] $menuBuilder.AppendLine( '</div>' )
+
+if( -not (Test-Path $DestinationPath -PathType Container) )
+{
+    New-Item $DestinationPath -ItemType Directory -Force 
+}
+
+@"
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <title>{0}</title>
+	<link href="styles.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+    {1}
+</body>
+</html>
+"@ -f $config.Title,$menuBuilder.ToString() | Out-File -FilePath (Join-Path $DestinationPath index.html) -Encoding OEM
+
+Join-Path $PSScriptRoot 'Resources\styles.css' | Get-Item | Copy-Item -Destination $DestinationPath
+
+$commands | 
+    #Where-Object { $_.Name -eq 'Get-Certificate' } | 
+    Get-Help -Full | 
+    Convert-HelpToHtml -Menu $menuBuilder.ToString() -Config $config -DestinationPath $DestinationPath
